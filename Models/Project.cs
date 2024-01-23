84f108eea542b07664f6d586047f5eaa649570de
rocketbing <156036235+rocketbing@users.noreply.github.com>
2024-01-22 20:01:46 -0500

    Add files via upload

---
+++ Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Class_Exercise_1.Models
+{
+	public class Project
+	{
+		//Primary Key
+		public int ProjectId { get; set; }
+
+		[Required]
+		public required string Name { get; set; }
+
+		//nullable
+		public string? Description { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+		public string? Status { get; set; }
+
+	}
+}
+
