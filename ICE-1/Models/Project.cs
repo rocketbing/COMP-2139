d030ada4722dcd4d53e189dc93de05a64412e378
rocketbing <156036235+rocketbing@users.noreply.github.com>
2024-02-10 20:54:31 -0500

    Add files via upload

---
+++ ICE-1/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿// GitHub Repository Link: https://github.com/rocketbing/COMP2139-In-Class-Exercise-1.git
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Class_Exercise_1.Models
+{
+	public class Project
+	{
+		//Primary Key
+		public int ProjectId { get; set; }
+
+		[Required]
+		public required string Name { get; set; }
+
+		//nullable
+		public string? Description { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+		public string? Status { get; set; }
+
+	}
+}
+
