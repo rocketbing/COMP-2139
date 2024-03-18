1f26f431dc1185cd29e4ca99ebe46b18f2cc4ecc
rocketbing <156036235+rocketbing@users.noreply.github.com>
2024-03-18 16:49:09 -0400

    Add files via upload

---
+++ ICE-3/Class Exercise 1/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Class_Exercise_1.Data;
+namespace Class_Exercise_1;
+using Microsoft.EntityFrameworkCore;
+using Class_Exercise_1.Data;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using Class_Exercise_1.Services;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
+        builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseStatusCodePagesWithRedirects("/Home/NotFound?statusCode={0}");
+            app.UseHsts();
+        }
+        else
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.MapRazorPages();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Projects}/{action=Index}/{id?}");
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
+
